003d064c18a27bb5b7bd5f578333ae7c608f44a8
Hariprasath <balaprasad24@gmail.com>
2020-05-09 23:46:12 +0530

    Day 9: Valid Square Root problem
    Signed-off-by: Hariprasath <balaprasad24@gmail.com>

---
+++ MayChallenge/Week2/9SquareRoot.java
@@ -0,0 +1,39 @@
+/**
+    Valid Perfect Square
+    --------------------
+    Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+    Note: Do not use any built-in library function such as sqrt.
+
+    Example 1:
+        Input: 16
+        Output: true
+    
+    Example 2:
+        Input: 14
+        Output: false
+ */
+ class ValidSquareRoot {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1)
+            return true;
+        if (num < 4)
+            return false;
+
+        int r = num;
+        int i = 0;
+        long mid = 0;
+        long midSqr = 0;
+        while (i <= num) {
+            mid = (i + num) / 2;
+            midSqr = mid * mid;
+            if (midSqr == r) {
+                return true;
+            } if (midSqr > r) {
+                num = (int) mid - 1;
+            } else {
+                i = (int) mid + 1;
+            }
+        }
+        return false;}
+}
